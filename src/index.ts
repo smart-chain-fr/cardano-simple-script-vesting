987624162516870b7f98dd0022bf4d6c2526b536
Amir Hossein Rad <ahmr10@gmail.com>
2022-05-28 15:03:51 +0100

    Fix initialization bug

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { initLucid, lockUtxo, redeemUtxo } from "./util";
 
-initLucid();
+await initLucid();
 
 const pass = 12344321;
 console.log("Datum pass: ", pass);
 
 document.getElementById("lock")?.addEventListener("click", () => {
-  // const userres = prompt("Enter amount to lock in Ada");
-  const userres = "10";
+  const userres = prompt("Enter amount to lock in Ada");
+  // const userres = "10";
   if (userres) {
     const ada = parseInt(userres, 10);
     console.log(ada);
