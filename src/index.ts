ccc921cfd208fccd2610a352086ddc4fe5756657
Amir Hossein Rad <ahmr10@gmail.com>
2022-06-05 15:57:25 +0100

    Draft filtering logic

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { Lucid, Blockfrost } from "lucid-cardano";
-import { lockUtxo, redeemUtxo } from "./util";
+import { Lucid, Blockfrost, PaymentKeyHash, UTxO } from "lucid-cardano";
+import { lockUtxo, matchingNumberAddress, redeemUtxo } from "./util";
 
 declare global {
-// eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line no-unused-vars
   interface Window {
     cardano: any;
   }
@@ -21,7 +21,66 @@ const init = async (blockfrostUrl: string, projectId: string, pass: number) => {
   return {
     lockTest: () => lockUtxo(lucid)(pass, BigInt(10000000)),
     redeemTest: () => redeemUtxo(lucid)(pass),
+    claimFunds: (pkh: string) =>
+      claimVestedFunds(lucid)({
+        [matchingNumberAddress(lucid)]: [{
+          nativeScript: { unlockTime: 1654267530, pkh },
+          asset: { currencySymbol: "", tokenName: "" },
+        }],
+      }),
   };
+};
+
+type ToClaim = {
+  [key: string]: {
+    nativeScript: { pkh: string; unlockTime: number };
+    asset: { currencySymbol: string; tokenName: string };
+  }[];
+};
+
+const claimChecks =
+  (lucid: Lucid) =>
+  (
+    pkh: PaymentKeyHash,
+    _unlockTime: number,
+    _assets: { currencySymbol: string; tokenName: string }
+  ): ((u: UTxO) => boolean)[] =>
+    [
+      // pkh check
+      (u) =>
+        lucid.utils.getAddressDetails(u.address).paymentCredential?.hash ===
+        pkh,
+      // unlock time check
+      (_u) => true,
+      // assetlcass check
+      (_u) => true,
+    ];
+
+const claimVestedFunds = (lucid: Lucid) => async (toClaim: ToClaim) => {
+  const utxosAggregated = await Promise.all(
+    Object.entries(toClaim).map(async ([address, tcs]) =>
+      Promise.all(
+        tcs.map(async (tc) => {
+          const utxos = (await lucid.utxosAt(address)).map(u => ({...u, addressDetails: lucid.utils.getAddressDetails(u.address)}));
+          console.log(utxos)
+
+          const predicates = claimChecks(lucid)(
+            tc.nativeScript.pkh,
+            tc.nativeScript.unlockTime,
+            tc.asset
+          );
+
+          const claimableUtxos = utxos.filter((utxo) =>
+            predicates.reduce((acc: boolean, predicate) => acc && predicate(utxo), true)
+          );
+          return claimableUtxos;
+        })
+      )
+    )
+  );
+
+  console.log(utxosAggregated);
+  // const tx = lucid.newTx().collectFrom();
 };
 
 export default init;
